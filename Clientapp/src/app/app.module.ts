c498565af4a973284af8fce39119817da08faa02
keshav-nandhan <keshavthehellrider@gmail.com>
2018-10-09 01:22:40 +0530

    Event Management scratch

---
+++ Clientapp/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderNavComponent } from './header-nav/header-nav.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderNavComponent
+  ],
+  imports: [
+    BrowserModule,
+    MDBBootstrapModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
